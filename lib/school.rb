fa34235b1243e069b7946c28e2ad40fa37cbe846
Susan Tan <ssusantan9@gmail.com>
2019-04-04 02:55:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  
+  attr_reader :roster, :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+end 
+
+def add_student(student_name, grade)
+ if @roster.has_key?(grade) 
+  @roster[grade] << student_name
+else
+  @roster[grade] = [student_name]
+end 
+end 
+
+
+
+end 
